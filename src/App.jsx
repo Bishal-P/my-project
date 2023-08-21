0146a87888ccccdbf30a8b8c609db967a76867ef
Bishal P <bishalprasad251122@gmail.com>
2023-08-21 13:44:36 +0530

    hoooo

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+// import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ParticleBg from './components/ParticleBg'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <ParticleBg>
+
+          
+        </ParticleBg>
+      <div className='Main'>
+        <img className='rounded-full' src="./images/back1.jpg" alt="" />
+        
+       
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+    
+      <div className='letter'>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae quo, dolor quia voluptatibus placeat rem. Asperiores quam cum dicta! Minima non, ipsum accusamus earum hic dolore facilis officia harum, enim, architecto blanditiis! Consequuntur animi in cum eius maiores a assumenda, numquam ab possimus nobis porro, ea sed quia, velit ut? Laborum fuga fugit ad, tempora repellat modi id iste cumque placeat exercitationem voluptatibus sit velit non voluptates commodi. Reprehenderit dolore possimus expedita et eaque. Quis, distinctio iste, alias, quia nihil voluptatem quos facilis praesentium dignissimos id impedit voluptatibus animi! Quae, doloremque! Iusto ullam mollitia fuga officiis, eveniet, est maiores esse optio fugit dolore saepe deserunt architecto? Necessitatibus eveniet quo dolorum soluta asperiores eum, at praesentium, perferendis sit iure, totam excepturi culpa atque quidem iste sequi laboriosam quae quaerat unde veritatis nesciunt ex. Quis sunt repellendus dolores libero similique in, recusandae exercitationem, at nam veritatis ut. Tenetur magnam mollitia aliquid quidem unde quasi dignissimos quis, velit similique accusantium deleniti obcaecati molestias, maxime illo enim dicta ipsum cumque quia incidunt, perferendis voluptatibus earum. Possimus, cumque distinctio earum architecto recusandae fuga maxime deleniti illum, rem porro sit corrupti vero dicta pariatur, vitae commodi suscipit labore fugiat? Eos enim consectetur animi maiores quia eum similique, tenetur repudiandae fugiat tempore quasi obcaecati praesentium sed officiis distinctio a corporis voluptatum nesciunt quaerat ex? In accusantium corporis aperiam dolore impedit nulla modi, repellat incidunt molestias atque earum quam sapiente laudantium unde esse nam, quasi voluptatibus veniam? Optio cum culpa enim sint excepturi veniam quod illum, eius alias accusantium. Magni pariatur sed, asperiores veritatis aspernatur neque? Quod dignissimos, itaque libero dolor rerum amet adipisci a maiores nulla nemo ex delectus magnam, temporibus dolores impedit ratione eos commodi? Deserunt iusto voluptatum dolores dolor consequuntur nesciunt, quis nemo nihil accusamus, odit iste? Nihil enim officia quasi recusandae suscipit exercitationem nesciunt soluta corrupti. Omnis obcaecati sit sint autem quas praesentium velit animi nihil earum reiciendis voluptate maxime consequuntur, maiores ad soluta! Delectus, repellat. Totam nulla expedita fugit deleniti sequi quas, dolore veniam maxime, vero eligendi voluptate et, assumenda sed nobis culpa consectetur placeat tempore debitis qui? Asperiores dolorum corrupti esse consequatur, adipisci necessitatibus voluptatum nesciunt pariatur, perspiciatis sequi, magni cupiditate aliquid doloribus tenetur at voluptas iure obcaecati fuga. Fugit reiciendis, unde iusto explicabo voluptates id nobis harum quidem temporibus eligendi! Labore quas assumenda dolor magni totam est, autem fuga, id voluptatem eius quaerat laudantium dolorem! Asperiores omnis accusamus pariatur aliquam porro! Officiis veniam eum quae quo inventore nesciunt aspernatur id corrupti rerum molestiae, dignissimos, facilis est? Natus quasi temporibus numquam esse, magni debitis animi aliquid porro sed veniam velit nesciunt repudiandae veritatis iusto dolore repellendus harum labore eum explicabo voluptas ad nisi dicta. Facere ab molestiae porro odit aut officiis labore quia, vitae, minus qui corporis repellat architecto, maiores fugiat dolorem eveniet. Fuga, dicta. Labore, maiores consectetur! Sint ab repudiandae vel quis voluptate iusto soluta, molestiae ea ex atque harum qui, sequi aut consequatur. Velit reprehenderit beatae exercitationem excepturi culpa perspiciatis, reiciendis vel illo repellat qui, sapiente accusantium esse quos possimus!
+
+      </div>
+    </>
+  )
+}
+
+export default App
